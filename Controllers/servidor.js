15325d620bc85045399416d77bfb9f239ae4cd9d
lucasAS1 <lucas.a.silva1@outlook.com>
2020-09-26 02:50:52 -0300

    criada primeira rota de RESTful, metodos do banco criado(necessaria correções)

--- Controllers/servidor.js
+++ Controllers/servidor.js
@@ -1,7 +1,12 @@
 const express=require("express");
+const bodyParser=require("body-parser");
+const DBCon=require("../Models/metodosBanco");
+
+const Parser=bodyParser.json();
 const app=express();
 
-
-app.get("/",(req,res)=>{
-    res.sendFile()    
+app.post("/save_cadastro",Parser,(req,res)=>{
+        DBCon.saveCadastro(req.body);
+        res.send("Cadastrado",201);
 })
+app.listen(5003);
