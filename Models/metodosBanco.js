1be370354d0e6ee2db5a1673b69eb264caf06617
lucasAS1 <lucas.a.silva1@outlook.com>
2023-04-05 00:05:21 -0300

    removendo bibliotecas locais e utilizando de cdn para que este repositório pare de fazer meu github ser 90+ js

--- Models/metodosBanco.js
+++ Models/metodosBanco.js
@@ -1,49 +1,48 @@
-mySql=require("mysql");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const con=mySql.createConnection({
-    host:"127.0.0.1",
-    port:"3308",
-    database:"desafio_node",
-    user:"atividadesWeb",
-    password:"df2"
-});
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxcWBQiDSBqxpZV_IZ_qvmd2-J8MY6EAg",
+  authDomain: "crudseila.firebaseapp.com",
+  projectId: "crudseila",
+  storageBucket: "crudseila.appspot.com",
+  messagingSenderId: "1014125663426",
+  appId: "1:1014125663426:web:15069c33658d895518975b"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
 
 function saveCadastro(cadastro){
-    let query=`insert into funcionarios(nome,cpf,telefone,email,id_departFK)
-    values("${cadastro.txtNome}",${cadastro.txtCPF},"${cadastro.txtTelefone}","${cadastro.txtEmail}",${cadastro.slcDepart})`;
-    con.query(query,(err)=>{
-        if(err) throw err;
-        console.log("Cadastro realizado com sucesso.");
-    });
+
 }
 
 function updateCadastro(cadastro){  
-    let query=`update funcionarios set nome="${cadastro.txtNome}",cpf=${cadastro.txtCPF},email="${cadastro.txtEmail}",id_departFK=${cadastro.slcDepart} where id_func=${cadastro.hdnId}`;
 
-    con.query(query,(err)=>{
-        if(err) throw err;
-        console.log("Update realizado com sucesso!")
-    });
 }
 
 async function viewCadastro(id){
-    let query="select * from View_inicio";
-    if (id !=null) query+=` where id_func=${id}`;
-    return new Promise((resolve,reject)=>{
-        con.query(query,(err,res)=>{
-        if(err) throw err;
-        resolve(res);
-    })});
+
+
     
 }
 
 function deleteCadastro(cad){
-    let query=`delete from funcionarios where id_func=${cad.id}`
-    con.query(query,(err)=>{
-        if(err) throw err;
-        console.log("Cadastro deletado com sucesso!");
-    })
+
 }
 
 exports.saveCadastro=saveCadastro;
